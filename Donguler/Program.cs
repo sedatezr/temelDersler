3d6dcfb61ce57d616cea33ac6c0caabcd51f1d86
sedatezr <92883119+sedatezr@users.noreply.github.com>
2025-10-22 08:56:41 +0300

    manevratik komutlar eklendi döngüler güncellendi

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Donguler
+﻿using System.Security.Cryptography;
+
+namespace Donguler
 {
     internal class Program
     {
@@ -19,11 +21,101 @@
             #endregion
 
 
+            #region for döngüsüyle birden 10' kadar olan sayılardan 2 ' ile bölünebilen çift sayıları ekrana yazdırma
+            //for(int i = 0; i<=10; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region for döngüsüyle 10 dan başlayıp 0' kadar eksilterek git
+            //for(int k =10; k>0; k--)
+            //{
+            //    Console.WriteLine(k);
+            //}
+            #endregion
+
+            #region 1 ile 40 arasında olan sayıların toplayarak sonuca yazdırmak
+
+            #endregion
+            //int toplam = 0;
+            //for (int j = 1; j<=40; j ++)
+            //{
+            //    if (j % 2 == 0)
+            //    {
+            //        toplam += j;
+            //    }
+            //}
+            //Console.WriteLine(toplam);
 
 
 
+            #region klavyeden girilen sayıların n faktöriyel verme
 
+            //Console.WriteLine("lütfen bir sayı giriniz");
+            //int gelenSayi = int.Parse(Console.ReadLine());
+
+
+            //int islem = 1;
+            //for (int i = gelenSayi; i > 0; i--) {
+
+            //    islem *= i;
+
+            //}
+            //Console.WriteLine(islem);
+
+            #endregion
+
+
+            /// WHİLE DÖNGÜSÜ ///
+            /// WHİLE DÖNGÜSÜ SONSUZ DÖNGÜLERDE KULLANILIR. BİR ŞART VERİLİR VE ŞART SAĞLANANA KADAR SONSUZ BİR DÖNGÜ İLE ÇALIŞMA YAPAR.
+            /// 
+
+
+            #region while döngüsü ile 1 den 10 kadar olan sayıları yazdırma
+            //int i = 0;
+
+            //while (i < 10)
+            //{
+            //    Console.WriteLine("merhaba while");
+            //    i++;
+            //}
+            #endregion
+
+            #region while döngüsü ile klavyeden girilen sayıyı geriye doğru 0 kadar sayan bir sayaç hazırlayalım
+            //int i = 5;
+
+            //while (i > 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+            #endregion
+
+
+            #region while döngüsü ile 0 ile 100 arasınd"a olan tek sayıların toplamını ekrana yazdırma
+            //int i = 1;
+            //int toplam = 0;
+
+            //while (i<=100)
+            //{
+            //    toplam += i;
+            //    i+=2;
+            //}
+            //Console.WriteLine(toplam);
+            #endregion
+
+            #region while döngüsü ile tarih saniye aralığını alarak her 5 in kaı olduğu sürece ekrana yazdırsın
+
+            while (true)
+            {
+                if(DateTime.Now.Second % 5 == 0)
+                {
+                    Console.WriteLine(DateTime.Now);
+                }
+            }
+            #endregion
         }
     }
-}
+} 
 
---
+++ YardımciManevratikKomutlar/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace YardımciManevratikKomutlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manevratik komutlar");
+            //kod yazarken belirli durumlarda döngüleri sonlandırmak veya devam edebilmeye ihtiyaç duyabiliyoruz. bu aşamada da manevratik komutlar devreye girmektedir. örneğin return kod bloğunu tamamen sonlandırır. continue devam eder. go to kendinden önceki kod satırına atlar gibi keywordler mevcuttur.
+
+
+            ///BREAK
+            ///break komutu döngü ve switch caselerde kullanılabilen bir manevratik komuttur. bu komut sayesinde var olan döngü sonlanır ve işlem biter. metod içerisinde kullanılması durumunda da metod sonlanır ve çıkış yapılır. yani çıkış yapmak istediğimiz alanlarda kullanabilmekteyiz.
+            ///
+
+            //while (true)
+            //{
+
+
+
+            //    if (DateTime.Now.Second == 10)
+            //    {
+
+            //        break;
+            //    }
+            //    Console.WriteLine("merhaba dünya");
+
+
+            //}
+
+
+
+            //klavyeden t harfi girene kadar girilen bütün sayıları toplayan sonsuz döngüyü yapınız
+            //int toplam = 0;
+            //while(true)
+            //{
+            //    Console.WriteLine("lütfen bir sayı giriniz");
+            //    string gelenSayi = Console.ReadLine();
+
+            //    if (gelenSayi== "t")
+            //    {
+            //        Console.WriteLine(toplam);
+            //        break;
+
+            //    }
+            //    else
+            //    {
+
+            //       int sayi = int.Parse(gelenSayi);
+            //        toplam += sayi;
+            //    }
+            //}
+
+
+
+
+            //Klavyeden girilen sayı 37 'nin katı ise program sonlansın. değil ise devam etsin.
+
+            //while (true)
+            //{
+            //    Console.WriteLine("lütfen bir sayı girinz");
+            //    int kullaniciSayi = int.Parse(Console.ReadLine());
+            //    if (kullaniciSayi % 37 == 0)
+            //    {
+            //        Console.WriteLine($"girilen sayı 37'nin katıdır. program sonlanıyor... {kullaniciSayi}");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("37 katı değil");
+            //    }
+            //}
+
+
+
+            ///CONTİNUE
+            ///continue sadece döngülerde kullanılabilinmektedir. kullanılıan alanlarda burayı es geç veya devam et şeklinde kullanılabilir.
+            ///00;
+
+            //örnek sayı 5 ise döngü devam etsin 5 değilse döngü sonlansın 
+            //while (true)
+            //{
+            //    Console.WriteLine("lütfen bir sayı giriniz");
+            //    int i = int.Parse(Console.ReadLine());
+            //    if(i == 5)
+            //{
+            //    Console.WriteLine("sayı 5 oldu");
+            //    continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sayı 5 değil");
+            //        break;
+            //    }
+            // }
+
+            //girilen sayı 5 ten küçükse program devam etsin 5 ten büyükse üstüne ekleyerek devam etsin 
+            //int toplam = 0;
+
+            //    while (true) {
+
+            //        Console.WriteLine("lütfen bir sayı giriniz");
+            //        int c = int.Parse(Console.ReadLine());
+
+            //    if(c > 5)
+            //    {
+            //        toplam += c;
+            //        Console.WriteLine(toplam);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("girdiğiniz sayı 5 'ten küçük olamaz lütfen tekrar deneyiniz");
+            //        continue;
+            //    }
+            //    }
+
+
+            //girilen değer toplam ise sonucu göster ve sonlandır değilse çarp devam et
+            int carpim = 1;
+            while (true)
+            {
+                Console.WriteLine("bir sayı giriniz girdiğiniz sayı pozitif ise çarpar 't' bastığında bitirir ve sonucu verir");
+
+                string gelendeger = Console.ReadLine();
+                if( gelendeger == "t")
+                {
+                    Console.WriteLine(carpim);
+                    return;
+
+                }else if (int.Parse(gelendeger) > 0)
+                {
+                    Console.WriteLine("devam");
+                    carpim *= int.Parse(gelendeger);
+                    continue;
+                }
+            }
+
+
+            }
+        }
+    }
+
